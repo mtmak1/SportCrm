f8b5135183ea72127e78138ed6487355d8c9be38
Arthur Makaev <unium.orb.1@gmail.com>
2021-03-01 10:02:57 +0500

    Add project files.

---
+++ WebApplication3/Models/PersonInformationModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication3.Models
+{
+    public class PersonInformationModel
+    {
+        public string Surname { get; set; }
+        public string Name { get; set; }
+        public DateTime BirthDay { get; set; }
+        public struct Address
+        {
+            public Address(string street,string home,string apartment)
+            {
+                Street = street;
+                Home = home;
+                Apartment = apartment;
+            }
+
+            public string Street { get; set; }
+            public string Home { get; set; }
+            public string Apartment { get; set; }
+
+            public override string ToString() => $"({Street}, {Home}, {Apartment})";
+        }
+
+    }
+}
